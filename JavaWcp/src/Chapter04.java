738ac9f11360d0ddbe8709e6f9ac49a45e495aba
yooske <yooske0214@gmail.com>
2022-11-27 14:52:27 +0000

    [add] javaWcp chapter2~4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 extends Variable02 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "Sakiyama Yosuke";
+		System.out.println(str1);
+
+		str1 = "Thank You!";
+		System.out.println(str1);
+
+		final int LUCKY_NUMBER = 214;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
