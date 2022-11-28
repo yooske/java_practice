fc968894dcf8487cdd7df9b409bf5ea4e9017e5c
yooske <yooske0214@gmail.com>
2022-11-28 04:02:16 +0000

    [add] JavaWcp chapter5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 9223372036854775807L;
+		double double1 = 1234567.89;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float float1 = 0.123f;
+		Integer integer1 = 5;
+		Character character1 = 'あ';
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+
+		int[] int1 = { 10, 20, 30, 40, 50 };
+		System.out.println(int1[3]);
+	}
+}
