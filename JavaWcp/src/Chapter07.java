7a93777ff483526bc5fdb23c7119d2e871eb4ed8
yooske <yooske0214@gmail.com>
2022-11-29 03:32:06 +0000

    [add] JavaWcp chapter7

---
+++ JavaWcp/src/Chapter07.java
@@ -0,0 +1,29 @@
+public class Chapter07 {
+	public static void main(String[] args) {
+		int dis = 3;
+		if (dis <= 5) {
+			System.out.println("とても近いです");
+		} else if (dis <= 10) {
+			System.out.println("近いです");
+		} else if (dis <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+
+		String color = "purple";
+		switch (color) {
+		case "red":
+			System.out.println("赤信号です");
+			break;
+		case "yellow":
+			System.out.println("黄信号です");
+			break;
+		case "blue":
+			System.out.println("青信号です");
+			break;
+		default:
+			System.out.println("信号の色ではありません");
+		}
+	}
+}
